4b36349d89bb95367cb15f305c9133340254f4b5
Kyuwoong Lee <kwooler@gmail.com>
2023-01-21 20:01:49 -0800

    Initial commit

---
+++ main_test.py
@@ -0,0 +1,46 @@
+import main
+import io
+import sys
+import re
+
+
+def test_main_1():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = '90\n10\n50\n40\n30'
+    datastr = '1 2 2 4 2\n2'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    regex_string = r'[\w,\W]*3'
+    regex_string += r'[\w,\W]*'
+    print(regex_string)
+    res = re.search(regex_string, lines[0])
+    assert res != None
+    print(res.group())
+
+
+def test_main_2():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = '90\n10\n50\n40\n30'
+    datastr = '3 3 3 3 3 3 3 3 3 3\n3'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    regex_string = r'[\w,\W]*10'
+    regex_string += r'[\w,\W]*'
+    print(regex_string)
+    res = re.search(regex_string, lines[0])
+    assert res != None
+    print(res.group())
